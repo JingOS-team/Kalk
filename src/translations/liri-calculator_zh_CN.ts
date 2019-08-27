7b26972e393f0f31ef83050fc1000dddb3353362
Liri CI <ci@liri.io>
2019-08-27 02:45:38 +0000

    Automatic merge of Transifex translations
    [ci skip]

---
+++ src/translations/liri-calculator_zh_CN.ts
@@ -0,0 +1,69 @@
+<?xml version="1.0" ?><!DOCTYPE TS><TS language="zh_CN" version="2.1">
+<context>
+    <name>CalculationLine</name>
+    <message>
+        <location filename="../ui/CalculationLine.qml" line="72"/>
+        <source>Error</source>
+        <translation>错误</translation>
+    </message>
+</context>
+<context>
+    <name>CalculationZone</name>
+    <message>
+        <location filename="../ui/CalculationZone.qml" line="56"/>
+        <source>Help</source>
+        <translation>帮助</translation>
+    </message>
+    <message>
+        <location filename="../ui/CalculationZone.qml" line="64"/>
+        <source>Open file</source>
+        <translation>打开文件</translation>
+    </message>
+    <message>
+        <location filename="../ui/CalculationZone.qml" line="74"/>
+        <source>Save file</source>
+        <translation>保存文件</translation>
+    </message>
+    <message>
+        <location filename="../ui/CalculationZone.qml" line="82"/>
+        <source>Advanced mode</source>
+        <translation>高级模式</translation>
+    </message>
+    <message>
+        <location filename="../ui/CalculationZone.qml" line="90"/>
+        <source>Close advanced mode</source>
+        <translation>关闭高级模式</translation>
+    </message>
+    <message>
+        <location filename="../ui/CalculationZone.qml" line="98"/>
+        <source>Toggle history</source>
+        <translation>历史</translation>
+    </message>
+    <message>
+        <location filename="../ui/CalculationZone.qml" line="106"/>
+        <source>Toggle expanded</source>
+        <translation>扩展</translation>
+    </message>
+</context>
+<context>
+    <name>FileHandler</name>
+    <message>
+        <location filename="../filehandler/filehandler.cpp" line="85"/>
+        <source>Cannot save: </source>
+        <translation>无法保存：</translation>
+    </message>
+</context>
+<context>
+    <name>Main</name>
+    <message>
+        <location filename="../ui/Main.qml" line="173"/>
+        <source>Discard unsaved?</source>
+        <translation>放弃保存？</translation>
+    </message>
+    <message>
+        <location filename="../ui/Main.qml" line="218"/>
+        <source>saved</source>
+        <translation>已保存</translation>
+    </message>
+</context>
+</TS>
